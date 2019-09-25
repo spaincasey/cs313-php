98319b9e186cc9d51c2b793ab05c91c596206385
Casey Spain <spaincasey7@gmail.com>
2019-09-24 20:38:39 -0600

    Added a top navigation bar

---
+++ web/week02/individual/personalhomepage.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html class="no-js">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Hello World</title>
+        <meta name="description" content="Hello World Assignment">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="personalhomepage.css">
+        <script src="personalhomepage.js"></script>
+    </head>
+    <body>
+        <div class="topnav">
+            <a class="active" href="#home">Home</a>
+            <a href="#news">News</a>
+            <a href="#contact">Contact</a>
+            <a href="#about">About</a>
+        </div>
+    </body>
+</html>
