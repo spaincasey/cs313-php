e32df674c2a581bbe41b6a1560a1720ae1338e48
Casey Spain <spaincasey7@gmail.com>
2019-09-24 20:43:35 -0600

    Changed Title

--- web/week02/individual/personalhomepage.php
+++ web/week02/individual/personalhomepage.php
@@ -3,13 +3,16 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Hello World</title>
+        <title>Homepage</title>
         <meta name="description" content="Hello World Assignment">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="personalhomepage.css">
         <script src="personalhomepage.js"></script>
     </head>
     <body>
+        <div>
+
+        </div>
         <div class="topnav">
             <a class="active" href="#home">Home</a>
             <a href="#news">News</a>
