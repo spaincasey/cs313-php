5020091171bf7877befadb8004832799f0be18fb
Casey Spain <spaincasey7@gmail.com>
2019-09-26 15:56:12 -0600

    border

--- web/week02/individual/assignments.php
+++ web/week02/individual/assignments.php
@@ -17,7 +17,7 @@
     <div class="jumbotron">
         <h1>CS313 Homepage</h1>
     </div>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light border">
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
