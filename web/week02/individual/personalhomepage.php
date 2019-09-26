e954adee2b955f518b99f6aff35ef949b7eb8ddc
Casey Spain <spaincasey7@gmail.com>
2019-09-26 15:18:34 -0600

    rid of one navbar

--- web/week02/individual/personalhomepage.php
+++ web/week02/individual/personalhomepage.php
@@ -30,20 +30,6 @@
             <li class="nav-item">
                 <a class="nav-link" href="#">Link</a>
             </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Dropdown
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="#">Action</a>
-                <a class="dropdown-item" href="#">Another action</a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#">Something else here</a>
-                </div>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link disabled" href="#">Disabled</a>
-            </li>
             </ul>
             <form class="form-inline my-2 my-lg-0">
             <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
@@ -51,10 +37,10 @@
             </form>
         </div>
     </nav>
-    <div class="topnav">
+    <!-- <div class="topnav">
         <a class="active" href="#home">Home</a>
         <a href="#assignments">Assignments</a>
-    </div>
+    </div> -->
     <div class="container">
         <div class="row">
             <div class="col-sm-4">
