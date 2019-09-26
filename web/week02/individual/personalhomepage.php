63855634070c439d8cd141929a0fbfd55ecd883a
Casey Spain <spaincasey7@gmail.com>
2019-09-26 15:15:08 -0600

    navbar

--- web/week02/individual/personalhomepage.php
+++ web/week02/individual/personalhomepage.php
@@ -16,14 +16,22 @@
     <div class="jumbotron">
         <h1>CS313 Homepage</h1>
     </div>
-    <nav class="navbar navbar-default">
+    <nav class="navbar navbar-inverse">
         <div class="container-fluid">
             <div class="navbar-header">
             <a class="navbar-brand" href="#">WebSiteName</a>
             </div>
             <ul class="nav navbar-nav">
             <li class="active"><a href="#">Home</a></li>
-            <li><a href="#">Page 1</a></li>
+            <li class="dropdown">
+                <a class="dropdown-toggle" data-toggle="dropdown" href="#">Page 1
+                <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                <li><a href="#">Page 1-1</a></li>
+                <li><a href="#">Page 1-2</a></li>
+                <li><a href="#">Page 1-3</a></li>
+                </ul>
+            </li>
             <li><a href="#">Page 2</a></li>
             <li><a href="#">Page 3</a></li>
             </ul>
