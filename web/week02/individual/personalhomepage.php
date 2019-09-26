1692232ae842cddc156c9331dd344441853d040f
Casey Spain <spaincasey7@gmail.com>
2019-09-26 16:02:23 -0600

    lots of changes

--- web/week02/individual/personalhomepage.php
+++ web/week02/individual/personalhomepage.php
@@ -14,7 +14,7 @@
     <link rel="stylesheet" href="personalhomepage.css">
   </head>
   <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light border">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
