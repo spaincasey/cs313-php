0f25a39d03d9a00d339f6031e3f34d26c38ec636
Casey Spain <spaincasey7@gmail.com>
2019-09-26 14:58:59 -0600

    added bootstrap

--- web/week02/individual/personalhomepage.php
+++ web/week02/individual/personalhomepage.php
@@ -1,25 +1,36 @@
-<!DOCTYPE html>
-<html class="no-js">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Homepage</title>
-        <meta name="description" content="Hello World Assignment">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="personalhomepage.css">
-        <script src="personalhomepage.js"></script>
-    </head>
-    <body>
-        <div class="jumbotron">
-            <h1>CS313 Homepage</h1>
-        </div>
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Homepage</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="Hello World Assignment">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="personalhomepage.css">
+    <script src="personalhomepage.js"></script>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="jumbotron">
+        <h1>CS313 Homepage</h1>
+    </div>
         
-        <!-- <div>
-            <img src="family_photo2.jpg" alt="Picture of my family" class="center">
-        </div> -->
-        <div class="topnav">
-            <a class="active" href="#home">Home</a>
-            <a href="#assignments">Assignments</a>
-        </div>
-    </body>
+    <!-- <div>
+        <img src="family_photo2.jpg" alt="Picture of my family" class="center">
+    </div> -->
+    <div class="topnav">
+        <a class="active" href="#home">Home</a>
+        <a href="#assignments">Assignments</a>
+    </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
 </html>
+
+        
+    
