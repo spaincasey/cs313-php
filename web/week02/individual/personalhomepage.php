8049a996e6fb9cdfa93228786d99f6f53b2e1028
Casey Spain <spaincasey7@gmail.com>
2019-09-26 15:13:26 -0600

    navbar fix

--- web/week02/individual/personalhomepage.php
+++ web/week02/individual/personalhomepage.php
@@ -16,7 +16,20 @@
     <div class="jumbotron">
         <h1>CS313 Homepage</h1>
     </div>
-    <div class="navbar navbar-default navbar-fixed-top">
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header">
+            <a class="navbar-brand" href="#">WebSiteName</a>
+            </div>
+            <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#">Page 1</a></li>
+            <li><a href="#">Page 2</a></li>
+            <li><a href="#">Page 3</a></li>
+            </ul>
+        </div>
+    </nav>
+    <div class="topnav">
         <a class="active" href="#home">Home</a>
         <a href="#assignments">Assignments</a>
     </div>
