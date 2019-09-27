674ceda9b831ac4b74b6703c9ec2bb113e407095
Casey Spain <spaincasey7@gmail.com>
2019-09-27 17:43:01 -0600

    c

--- web/week02/individual/personalhomepage.js
+++ web/week02/individual/personalhomepage.js
@@ -4,7 +4,7 @@ function comment(){
     //let txt = document.getElementById('example').value;
     // comments.append(txt);
     // for (var i = 0; i < comments.length; i++) { 
-    //document.getElementById('para').innerHTML = txt; 
+    document.getElementById('para').innerHTML = txt; 
     // }
     // // document.getElementById('para').innerHTML = comments; 
     // // $( "#para" ).empty().append( txt );
