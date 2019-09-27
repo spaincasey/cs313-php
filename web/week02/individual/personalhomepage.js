2eab7b58e183867b09532264f91206a4c37c6c49
Casey Spain <spaincasey7@gmail.com>
2019-09-27 16:57:36 -0600

    for loop

--- web/week02/individual/personalhomepage.js
+++ web/week02/individual/personalhomepage.js
@@ -2,6 +2,9 @@ function comment(){
     var comments = [];   
     var txt = document.getElementById('example').value   
     comments.append(txt);
+    for (var i = 0; i < comments.length; i++) { 
+        document.getElementById('para').innerHTML = comments[i]; 
+    }
     document.getElementById('para').innerHTML = comments; 
     $( "#para" ).empty().append( txt );
 }
