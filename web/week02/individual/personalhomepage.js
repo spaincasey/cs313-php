cdb4e986f096f5168af10c1dc22e622250aa563c
Casey Spain <spaincasey7@gmail.com>
2019-09-27 16:54:31 -0600

    comments

--- web/week02/individual/personalhomepage.js
+++ web/week02/individual/personalhomepage.js
@@ -1,4 +1,7 @@
-function comment(){   
-    var txt = document.getElementById('example').value    
+function comment(){
+    var comments = [];   
+    var txt = document.getElementById('example').value   
+    comments.append(txt);
+    document.getElementById('para').innerHTML = comments; 
     $( "#para" ).empty().append( txt );
 }
