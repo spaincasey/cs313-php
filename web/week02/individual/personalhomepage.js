c3b3595c8262da99042d342fc52aec16023acd92
Casey Spain <spaincasey7@gmail.com>
2019-09-27 17:40:07 -0600

    commit after crash

--- web/week02/individual/personalhomepage.js
+++ web/week02/individual/personalhomepage.js
@@ -1,12 +1,11 @@
-<script>
 function comment(){
+    let txt = document.querySelector("#example").value;
     // var comments = [];   
-    var txt = document.getElementById('example').value;
+    //let txt = document.getElementById('example').value;
     // comments.append(txt);
     // for (var i = 0; i < comments.length; i++) { 
-    document.getElementById('para').innerHTML = txt; 
+    //document.getElementById('para').innerHTML = txt; 
     // }
     // // document.getElementById('para').innerHTML = comments; 
     // // $( "#para" ).empty().append( txt );
-};
-</script>
+}
