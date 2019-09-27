c7c365af647aa5c355c3c7053768ba1c39e713db
Casey Spain <spaincasey7@gmail.com>
2019-09-27 17:47:31 -0600

    multiple comments

--- web/week02/individual/personalhomepage.js
+++ web/week02/individual/personalhomepage.js
@@ -4,7 +4,7 @@ function comment(){
     //let txt = document.getElementById('example').value;
     comments.unshift(txt);
     for (var i = 0; i < comments.length; i++) { 
-        document.getElementById('para').innerHTML = txt + "<br>"; 
+        document.getElementById('para').innerHTML += txt + "<br>"; 
     }
     // // document.getElementById('para').innerHTML = comments; 
     // // $( "#para" ).empty().append( txt );
