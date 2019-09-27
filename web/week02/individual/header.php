6afda0da51df002cdf787f0b8f0a78d23f0bd318
Casey Spain <spaincasey7@gmail.com>
2019-09-27 15:47:14 -0600

    adding header to each page with php

---
+++ web/week02/individual/header.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+    <title><?php echo $title; ?></title>
+</head>
+<body>
+
+<h1>Our Web Site</h1>
+<!-- end header -->
--- web/week02/individual/personalhomepage.php
+++ web/week02/individual/personalhomepage.php
@@ -14,6 +14,9 @@
     <link rel="stylesheet" href="personalhomepage.css">
   </head>
   <body>
+    <?php
+    include "header.php";
+    ?>
     <nav class="navbar navbar-expand-lg navbar-light bg-light mr-auto">
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
