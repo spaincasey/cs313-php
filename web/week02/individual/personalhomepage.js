9d732f2f62756b116f4aa5498a5b0d3617d80377
Casey Spain <spaincasey7@gmail.com>
2019-09-27 17:03:33 -0600

    attempt to fix js

--- web/week02/individual/personalhomepage.js
+++ web/week02/individual/personalhomepage.js
@@ -1,10 +1,10 @@
 function comment(){
-    var comments = [];   
-    var txt = document.getElementById('example').value   
-    comments.append(txt);
-    for (var i = 0; i < comments.length; i++) { 
-        document.getElementById('para').innerHTML = "<p>" + txt + "</p>"; 
-    }
-    // document.getElementById('para').innerHTML = comments; 
-    // $( "#para" ).empty().append( txt );
+    // var comments = [];   
+    var txt = document.getElementById('example').value;
+    // comments.append(txt);
+    // for (var i = 0; i < comments.length; i++) { 
+    document.getElementById('para').innerHTML = txt; 
+    // }
+    // // document.getElementById('para').innerHTML = comments; 
+    // // $( "#para" ).empty().append( txt );
 }
