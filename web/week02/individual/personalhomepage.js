061cf3a4e67df64b42dbd02a820918694a99f59c
Casey Spain <spaincasey7@gmail.com>
2019-09-27 16:32:21 -0600

    javascript in seperate file

--- web/week02/individual/personalhomepage.js
+++ web/week02/individual/personalhomepage.js
@@ -1,7 +1,7 @@
-window.onclick = function(e)
-{   var id =  e.target.id;   
- if (id === 'sent')  
- { var txt = document.getElementById('example').value    
-   $( "#para" ).empty().append( txt );
- }
+window.onclick = function(e){   
+    var id =  e.target.id;   
+    if (id === 'sent')  { 
+        var txt = document.getElementById('example').value    
+        $( "#para" ).empty().append( txt );
+    }
 }
--- web/week02/individual/personalhomepage.php
+++ web/week02/individual/personalhomepage.php
@@ -32,16 +32,8 @@
     <?php
     include "footer.php";
     ?>
-    <script>
-        window.onclick = function(e){   
-            var id =  e.target.id;   
-            if (id === 'sent')  { 
-                var txt = document.getElementById('example').value    
-                $( "#para" ).empty().append( txt );
-            }
-        }
-    </script>
-
+    
+    <script type="text/javascript" src="personalhomepage.js"></script>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
