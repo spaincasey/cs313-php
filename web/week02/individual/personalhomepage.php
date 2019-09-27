d74546fe6a9414e1fb2f071c09743aeda8e17a56
Casey Spain <spaincasey7@gmail.com>
2019-09-27 16:08:02 -0600

    footer added in php

--- web/week02/individual/personalhomepage.php
+++ web/week02/individual/personalhomepage.php
@@ -27,15 +27,11 @@
         <br>
         <img src="Braves Game.jpg" class="img-fluid ${3|rounded-top,rounded-right,rounded-bottom,rounded-left,rounded-circle,|}" alt="">
     </div>
-    <div class="center">
-        <h4>Leave a comment</h4>
-        <label for="example">Enter Text
-        </label>
-        <input id="example" type="text"
-                name="Ntext" size="20">
-        <input id="sent" type="submit"  
-                value="Send"> 
-    </div>
+    <br>
+    <br>
+    <?php
+    include "footer.php";
+    ?>
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
