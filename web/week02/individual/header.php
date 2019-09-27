2ef2e30394ae5c9864fd23ab58a253515eeb3bf3
Casey Spain <spaincasey7@gmail.com>
2019-09-27 15:49:54 -0600

    navbar in php

--- web/week02/individual/header.php
+++ web/week02/individual/header.php
@@ -1,8 +1,34 @@
-<html>
-<head>
-    <title><?php echo $title; ?></title>
-</head>
+<html lang="en">
+  <head>
+    <title>Homepage</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="Hello World Assignment">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="personalhomepage.css">
+    <script src="personalhomepage.js"></script>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="personalhomepage.css">
+  </head>
 <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light mr-auto">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-<h1>Our Web Site</h1>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="assignments.php">Assignments</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</body>
+</html>
 <!-- end header -->
