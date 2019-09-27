6ed3650e678d12c6878d09e64bbda4213a6f6272
Casey Spain <spaincasey7@gmail.com>
2019-09-27 17:51:30 -0600

    comments are working

--- web/week02/individual/personalhomepage.js
+++ web/week02/individual/personalhomepage.js
@@ -1,11 +1,8 @@
 function comment(){
     var comments = []; 
     let txt = document.querySelector("#example").value;  
-    //let txt = document.getElementById('example').value;
     comments.unshift(txt);
     for (var i = 0; i < comments.length; i++) { 
         document.getElementById('para').innerHTML += txt + "<br>"; 
     }
-    // // document.getElementById('para').innerHTML = comments; 
-    // // $( "#para" ).empty().append( txt );
 }
