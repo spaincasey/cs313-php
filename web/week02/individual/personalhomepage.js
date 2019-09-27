ca4b02f09eb48ea571ce2cdaeffda5dc3a7e29f1
Casey Spain <spaincasey7@gmail.com>
2019-09-27 17:01:11 -0600

    <p>

--- web/week02/individual/personalhomepage.js
+++ web/week02/individual/personalhomepage.js
@@ -3,7 +3,7 @@ function comment(){
     var txt = document.getElementById('example').value   
     comments.append(txt);
     for (var i = 0; i < comments.length; i++) { 
-        document.getElementById('para').innerHTML = txt; 
+        document.getElementById('para').innerHTML = "<p>" + txt + "</p>"; 
     }
     // document.getElementById('para').innerHTML = comments; 
     // $( "#para" ).empty().append( txt );
