e5348739b05c2d673dcf6736ef454fba2d2e071a
Casey Spain <spaincasey7@gmail.com>
2019-10-01 19:15:30 -0600

    grid

--- web/week02/individual/assignments.php
+++ web/week02/individual/assignments.php
@@ -30,6 +30,28 @@
           One of three columns
         </div>
       </div>
+      <div class="row">
+        <div class="col-sm">
+          One of three columns
+        </div>
+        <div class="col-sm">
+          One of three columns
+        </div>
+        <div class="col-sm">
+          One of three columns
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-sm">
+          One of three columns
+        </div>
+        <div class="col-sm">
+          One of three columns
+        </div>
+        <div class="col-sm">
+          One of three columns
+        </div>
+      </div>
     </div>
     <?php
     include "footer.php";
