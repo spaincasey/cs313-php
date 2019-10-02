6c5393173b73f13bd49acb77e1760baa2a878d54
Casey Spain <spaincasey7@gmail.com>
2019-10-01 19:14:19 -0600

    bootstrap grid

--- web/week02/individual/assignments.php
+++ web/week02/individual/assignments.php
@@ -18,16 +18,18 @@
     include "header.php";
     ?>
     <br>
-    <div class="grid-container">
-      <div class="item1"><button type="button">Click Me!</button></div>
-      <div class="grid-item"><button type="button">Click Me!</button></div>
-      <div class="grid-item"><button type="button">Click Me!</button></div>
-      <div class="grid-item"><button type="button">Click Me!</button></div>
-      <div class="grid-item"><button type="button">Click Me!</button></div>
-      <div class="grid-item"><button type="button">Click Me!</button></div>
-      <div class="grid-item"><button type="button">Click Me!</button></div>
-      <div class="grid-item"><button type="button">Click Me!</button></div>
-      <div class="grid-item"><button type="button">Click Me!</button></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm">
+          One of three columns
+        </div>
+        <div class="col-sm">
+          One of three columns
+        </div>
+        <div class="col-sm">
+          One of three columns
+        </div>
+      </div>
     </div>
     <?php
     include "footer.php";
