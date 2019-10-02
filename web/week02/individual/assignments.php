a61c150f37ecb81851238131c78a6b6d141c0e47
Casey Spain <spaincasey7@gmail.com>
2019-10-01 19:17:31 -0600

    buttons

--- web/week02/individual/assignments.php
+++ web/week02/individual/assignments.php
@@ -21,35 +21,35 @@
     <div class="container">
       <div class="row">
         <div class="col-sm">
-          One of three columns
+          <button type="button" class="btn btn-primary">Primary</button>
         </div>
         <div class="col-sm">
-          One of three columns
+          <button type="button" class="btn btn-primary">Primary</button>
         </div>
         <div class="col-sm">
-          One of three columns
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-sm">
-          One of three columns
-        </div>
-        <div class="col-sm">
-          One of three columns
-        </div>
-        <div class="col-sm">
-          One of three columns
+          <button type="button" class="btn btn-primary">Primary</button>
         </div>
       </div>
       <div class="row">
         <div class="col-sm">
-          One of three columns
+          <button type="button" class="btn btn-primary">Primary</button>
         </div>
         <div class="col-sm">
-          One of three columns
+          <button type="button" class="btn btn-primary">Primary</button>
         </div>
         <div class="col-sm">
-          One of three columns
+          <button type="button" class="btn btn-primary">Primary</button>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-sm">
+          <button type="button" class="btn btn-primary">Primary</button>
+        </div>
+        <div class="col-sm">
+          <button type="button" class="btn btn-primary">Primary</button>
+        </div>
+        <div class="col-sm">
+          <button type="button" class="btn btn-primary">Primary</button>
         </div>
       </div>
     </div>
