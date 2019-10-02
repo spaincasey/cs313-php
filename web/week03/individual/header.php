4d37f0ff8d8025dbd0cdf97fec0323e997883a35
Casey Spain <spaincasey7@gmail.com>
2019-10-02 17:35:57 -0600

    cart button

--- web/week03/individual/header.php
+++ web/week03/individual/header.php
@@ -27,7 +27,7 @@
             </ul>
             <ul class="nav navbar-nav navbar-right">
                 <li class="nav-item">
-                    <img src="shopping_cart.png" class="icon" href="viewCart.php" alt="">    
+                    <button class="btn">Cart</button>   
                 </li>
             </ul>
         </div>
