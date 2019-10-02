198355d8886efe9b43989fa12bad88d92833383c
Casey Spain <spaincasey7@gmail.com>
2019-10-01 20:39:22 -0600

    change name

---
+++ web/week03/individual/browseItems.php
@@ -0,0 +1,75 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Homepage</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="Hello World Assignment">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- <link rel="stylesheet" href="personalhomepage.css"> -->
+    <script src="personalhomepage.js"></script>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="..\..\week02\individual\personalhomepage.css">
+  </head>
+  <body>
+    <?php
+    include "header.php";
+    ?>
+    <br>
+    <div class="container">
+      <div class="row">
+        <div class="col-4 d-flex justify-content-center">
+        <a href="..\..\week03\individual\shoppingCart.php" class="btn btn-info" role="button">Shopping Cart</a>
+        </div>
+        <div class="col-4 d-flex justify-content-center">
+          <button type="button" class="btn btn-primary">Assignment 2</button>
+        </div>
+        <div class="col-4 d-flex justify-content-center">
+          <button type="button" class="btn btn-primary">Assignment 3</button>
+        </div>
+      </div>
+      <br>
+      <div class="row">
+        <div class="col-4 d-flex justify-content-center">
+          <button type="button" class="btn btn-primary">Assignment 4</button>
+        </div>
+        <div class="col-4 d-flex justify-content-center">
+          <button type="button" class="btn btn-primary">Assignment 5</button>
+        </div>
+        <div class="col-4 d-flex justify-content-center">
+          <button type="button" class="btn btn-primary">Assignment 6</button>
+        </div>
+      </div>
+      <br>
+      <div class="row">
+        <div class="col-4 d-flex justify-content-center">
+          <button type="button" class="btn btn-primary">Assignment 7</button>
+        </div>
+        <div class="col-4 d-flex justify-content-center">
+          <button type="button" class="btn btn-primary">Assignment 8</button>
+        </div>
+        <div class="col-4 d-flex justify-content-center">
+          <button type="button" class="btn btn-primary">Assignment 9</button>
+        </div>
+      </div>
+    </div>
+    <br><br>
+    <br>
+    <br>
+    <?php
+    include "footer.php";
+    ?>
+    <br>
+    <br>
+    
+    <script type="text/javascript" src="personalhomepage.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
+
+        
+    
