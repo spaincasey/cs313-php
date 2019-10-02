89a2498dabd5bbdd33aa65f999a624fe6c93aa93
Casey Spain <spaincasey7@gmail.com>
2019-10-01 19:07:42 -0600

    buttons

--- web/week02/individual/assignments.php
+++ web/week02/individual/assignments.php
@@ -19,15 +19,15 @@
     ?>
     <br>
     <div class="grid-container">
-      <div class="grid-item">1</div>
-      <div class="grid-item">2</div>
-      <div class="grid-item">3</div>
-      <div class="grid-item">4</div>
-      <div class="grid-item">5</div>
-      <div class="grid-item">6</div>
-      <div class="grid-item">7</div>
-      <div class="grid-item">8</div>
-      <div class="grid-item">9</div>
+      <div class="grid-item"><button></div>
+      <div class="grid-item"><button></div>
+      <div class="grid-item"><button></div>
+      <div class="grid-item"><button></div>
+      <div class="grid-item"><button></div>
+      <div class="grid-item"><button></div>
+      <div class="grid-item"><button></div>
+      <div class="grid-item"><button></div>
+      <div class="grid-item"><button></div>
     </div>
     <?php
     include "footer.php";
