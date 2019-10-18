f7406f62c7833a0debeb649585ebbbf016a2fa33
Casey Spain <spaincasey7@gmail.com>
2019-10-17 21:50:17 -0600

    homepage

--- web/week05/individual/HomePage.php
+++ web/week05/individual/HomePage.php
@@ -14,7 +14,7 @@ if(empty($_SESSION['cart']))
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-  <link rel="stylesheet" href="shoppingCart.css">
+  <link rel="stylesheet" href="HomePage.css">
 </head>
 <body>
 
