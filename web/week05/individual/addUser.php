bdba683c1f630e98d70b506a9a47dced2638be44
Casey Spain <spaincasey7@gmail.com>
2019-10-29 18:52:10 -0600

    USER MESSAGE

--- web/week05/individual/addUser.php
+++ web/week05/individual/addUser.php
@@ -12,11 +12,17 @@ $q = $_REQUEST["q"];
 // {
 // //insert query goes here
 // }
+
+$statement = $db->prepare("SELECT EXISTS (SELECT * FROM User_app WHERE email = '$q')");
+$exists = $statement->execute();
+//select exists(select 1 from contact where id=12)
+if($exists){
+   echo 'alert(Your email is already registered)';
+}
+else
+{
     
 $statement = $db->prepare("INSERT INTO User_app(email)VALUES('$q')");
-$statement->execute();
-
-$statement = $db->prepare("SELECT email FROM User_app");
 $statement->execute();
 // Go through each result
 while ($row = $statement->fetch(PDO::FETCH_ASSOC))
@@ -25,8 +31,9 @@ while ($row = $statement->fetch(PDO::FETCH_ASSOC))
 	$email = $row['email'];
    // $image = $row['image'];
    console.log($email);
-//   echo "<h3>$email<h3>";
+   echo "<h3>$email<h3>";
 //   echo "<img src='$image' class='img-responsive' alt='Image'>";
+}
 }
 
 ?>
