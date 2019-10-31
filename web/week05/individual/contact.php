906487a28a4e5bdb7b04d2b7e41e33469ad2225b
Casey Spain <spaincasey7@gmail.com>
2019-10-31 15:36:10 -0600

    br

--- web/week05/individual/contact.php
+++ web/week05/individual/contact.php
@@ -105,7 +105,7 @@
 <div class='jobs'>
   <div class='jobs'>
     <h1>Contact Us</h1>
-    <br><br>
+    <br>
     <h3>We would love to work with you on your next construction project
     <br>contact us and we will get in touch with you as soon as possible</h3>
   </div>
