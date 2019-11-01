449accf639244e424fba979c2ae706189f03d737
Casey Spain <spaincasey7@gmail.com>
2019-11-01 14:25:46 -0600

    semicolon

--- web/week05/individual/loadReviews.php
+++ web/week05/individual/loadReviews.php
@@ -12,7 +12,7 @@ while ($row = $statement->fetch(PDO::FETCH_ASSOC))
     $user_app = $row['user_app_id'];
     $statement2 = $db->prepare("SELECT first_name, last_name FROM User_app WHERE id = '$user_app'");
     $statement2->execute();
-    $row2 = $statement2->fetch(PDO::FETCH_ASSOC)
+    $row2 = $statement2->fetch(PDO::FETCH_ASSOC);
     $name = $row2['first_name'] += " " += $row2['last_name'];
     // $name = '';
     // while ($row2 = $statement2->fetch(PDO::FETCH_ASSOC))
