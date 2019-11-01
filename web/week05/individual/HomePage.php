632febe512282ebc247b2ac939d5d9829ea3a8cc
Casey Spain <spaincasey7@gmail.com>
2019-11-01 15:01:45 -0600

    div jobs

--- web/week05/individual/HomePage.php
+++ web/week05/individual/HomePage.php
@@ -172,15 +172,17 @@
 </div>
 
 <footer>
-  <div class="row">
-    <div class="col-sm-4">
-      <p>Email - pannel@construction.com</p>
-    </div>
-    <div class="col-sm-4">
-      <p>Phone - 555-555-5555</p>
-    </div>
-    <div class="col-sm-4">
-      <p>Address - 123 N 456 S Rexburg, ID 83440</p>
+  <div class="jobs">
+    <div class="row">
+      <div class="col-sm-4">
+        <p>Email - pannel@construction.com</p>
+      </div>
+      <div class="col-sm-4">
+        <p>Phone - 555-555-5555</p>
+      </div>
+      <div class="col-sm-4">
+        <p>Address - 123 N 456 S Rexburg, ID 83440</p>
+      </div>
     </div>
   </div>
 </footer>
