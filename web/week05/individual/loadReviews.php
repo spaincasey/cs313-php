39a96f411ec5149dabf4bdd8e569d377eb846089
Casey Spain <spaincasey7@gmail.com>
2019-11-01 14:23:56 -0600

    name

--- web/week05/individual/loadReviews.php
+++ web/week05/individual/loadReviews.php
@@ -10,7 +10,7 @@ while ($row = $statement->fetch(PDO::FETCH_ASSOC))
 {
 	// $category = $row['category'];
     $user_app = $row['user_app_id'];
-    $statement2 = $db->prepare("SELECT * FROM User_app WHERE id = '$user_app'");
+    $statement2 = $db->prepare("SELECT first_name, last_name FROM User_app WHERE id = '$user_app'");
     $statement2->execute();
     // $name = '';
     while ($row2 = $statement2->fetch(PDO::FETCH_ASSOC))
