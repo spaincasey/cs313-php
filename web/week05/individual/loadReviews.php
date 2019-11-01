ee7a762659b7c1a73958b04b9ccdb0be8bada1b5
Casey Spain <spaincasey7@gmail.com>
2019-11-01 14:28:52 -0600

    user_app

--- web/week05/individual/loadReviews.php
+++ web/week05/individual/loadReviews.php
@@ -5,20 +5,20 @@ $db = get_db();
 
 $statement = $db->prepare("SELECT user_app_id, review_text FROM Review");
 $statement->execute();
+
 // Go through each result
 while ($row = $statement->fetch(PDO::FETCH_ASSOC))
 {
 	// $category = $row['category'];
     $user_app = $row['user_app_id'];
-    $statement2 = $db->prepare("SELECT first_name, last_name FROM User_app WHERE id = '$user_app'");
+    $statement2 = $db->prepare("SELECT first_name, last_name FROM User_app WHERE id = $user_app");
     $statement2->execute();
-    $row2 = $statement2->fetch(PDO::FETCH_ASSOC);
-    $name = $row2['first_name'] += " " += $row2['last_name'];
+    
     // $name = '';
-    // while ($row2 = $statement2->fetch(PDO::FETCH_ASSOC))
-    // {
-    //     $name = $row2['first_name'] + " " + $row2['last_name'];
-    // }
+    while ($row2 = $statement2->fetch(PDO::FETCH_ASSOC))
+    {
+        $name = $row2['first_name'] + " " + $row2['last_name'];
+    }
 	$review = $row['review_text'];
     echo "<h3>$name</h3>";
     echo "<p>$review</p>";
