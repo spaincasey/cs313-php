f436212d37619ad9ebc36e869a8febeb5b8669d6
Casey Spain <spaincasey7@gmail.com>
2019-11-01 14:34:54 -0600

    please work

--- web/week05/individual/loadReviews.php
+++ web/week05/individual/loadReviews.php
@@ -11,16 +11,18 @@ while ($row = $statement->fetch(PDO::FETCH_ASSOC))
 {
 	// $category = $row['category'];
     $user_app = $row['user_app_id'];
-    $statement2 = $db->prepare("SELECT first_name, last_name FROM User_app WHERE id = $user_app");
+    $statement2 = $db->prepare("SELECT first_name, last_name FROM User_app WHERE id = '$user_app'");
     $statement2->execute();
     
-    $name = '';
+    $fname = '';
+    $lname = '';
     while ($row2 = $statement2->fetch(PDO::FETCH_ASSOC))
     {
-        $name = $row2['first_name'] + " " + $row2['last_name'];
+        $fname = $row2['first_name'];
+        $lname = $row2['last_name'];
     }
 	$review = $row['review_text'];
-    echo "<h3>$name</h3>";
+    echo "<h3>$fname $lname</h3>";
     echo "<p>$review</p>";
 }
 ?>
