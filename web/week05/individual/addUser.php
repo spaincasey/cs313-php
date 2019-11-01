b6373d938baf1e0a32a546d1c7db55f4522fc39c
Casey Spain <spaincasey7@gmail.com>
2019-11-01 16:42:08 -0600

    error

--- web/week05/individual/addUser.php
+++ web/week05/individual/addUser.php
@@ -7,36 +7,40 @@ $db = get_db();
 $first = $_REQUEST["fname"];
 $last = $_REQUEST["lname"];
 $email = $_REQUEST["email"];
+echo '<script language="javascript">';
+echo 'alert("This is working")';
+echo '</script>';
 $statement = $db->prepare("SELECT email FROM User_app WHERE email = '$email'");
 $statement->execute();
-if ($_POST['action'] == 'Sign in') {
-    if (pg_num_rows($statement)) {
-      $_SESSION["name"] = $first += " " += $last;
-    }
-    else {
-      echo '<script language="javascript">';
-      echo 'alert("This email does not exist in our records")';
-      echo '</script>';
-    }
+if (pg_num_rows($statement)) {
+    echo '<script language="javascript">';
+    echo 'alert("This email is already registered")';
+    echo '</script>';
 }
-else if ($_POST['action'] == 'Sign up') {
-    if (pg_num_rows($statement)) {
-      echo '<script language="javascript">';
-      echo 'alert("This email is already registered")';
-      echo '</script>';
-    }
-    else {
-      echo '<script language="javascript">';
-      echo 'alert("This email does not exist in our records")';
-      echo '</script>';
-    }
+else {
+    $statement2 = $db->prepare("INSERT INTO User_app(first_name, last_name, email)VALUES('$first', '$last', '$email')");
+    $statement2->execute();
+    $_SESSION["name"] = $first += " " += $last;
 }
 
-$first = $_REQUEST["fname"];
-$last = $_REQUEST["lname"];
-$email = $_REQUEST["email"];
-$statement2 = $db->prepare("INSERT INTO User_app(first_name, last_name, email)VALUES('$first', '$last', '$email')");
-$statement2->execute();
-$_SESSION["name"] = $first += " " += $last;
+// else if ($_POST['action'] == 'Sign up') {
+//     if (pg_num_rows($statement)) {
+//       echo '<script language="javascript">';
+//       echo 'alert("This email is already registered")';
+//       echo '</script>';
+//     }
+//     else {
+//       echo '<script language="javascript">';
+//       echo 'alert("This email does not exist in our records")';
+//       echo '</script>';
+//     }
+// }
+
+// $first = $_REQUEST["fname"];
+// $last = $_REQUEST["lname"];
+// $email = $_REQUEST["email"];
+// $statement2 = $db->prepare("INSERT INTO User_app(first_name, last_name, email)VALUES('$first', '$last', '$email')");
+// $statement2->execute();
+// $_SESSION["name"] = $first += " " += $last;
 
 ?>
